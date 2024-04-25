3499b30da040a8f26957cbff055c57a510e8d09e
m11toh <martohmashah@gmail.com>
2024-04-25 08:42:42 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
+  <div id="about">
+    <h2>About me</h2>
+    <p>lorem ipsum</p>
+    <img src= {image} alt="I made this"/>
+  </div>
   return <div>About</div>;
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
+  <div>
+    <h1 style="firebrick"> Your {name} is a Web Developer from Your {city}</h1>
+  </div>
   return <div>Home</div>;
 }
 
